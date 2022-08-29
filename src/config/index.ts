3c03dbac834a61cf69d679629b6a9efa1c5b9755
Ivan García <miltonivan117@gmail.com>
2022-08-29 08:25:29 -0500

    CHANGE NAME

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,7 @@
 const CONFIG = {
   GRAPHQL_URL:
-    process.env.GRAPHQL_SERVER_URL || 'http://localhost:3000/api/graphql',
+    process.env.NEXT_PUBLIC_GRAPHQL_SERVER_URL ||
+    'http://localhost:3000/api/graphql',
   MONGODB: process.env.MONGODB_URI as string
 };
 

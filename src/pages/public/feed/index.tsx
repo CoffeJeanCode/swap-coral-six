e7efbb99ac18b3cb173320db8a36180dd3d302b9
Ivan García <miltonivan117@gmail.com>
2022-09-04 22:10:42 -0500

    LOADER

--- src/pages/public/feed/index.tsx
+++ src/pages/public/feed/index.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@apollo/client';
 import { LISTBYTYPE } from '@Apollo/client/query/listByType';
 import AtomCard from '@Components/@atoms/AtomCard';
+import AtomLoader from '@Components/@atoms/AtomLoader';
 import { AtomText } from '@Components/@atoms/AtomText';
 import AtomWrapper from '@Components/@atoms/Atomwrapper';
 import { css } from '@emotion/react';
@@ -10,7 +11,7 @@ import { useRouter } from 'next/router';
 
 const Public: NextPageFCProps = () => {
   const router = useRouter();
-  const { data } = useQuery<IQueryFilter<'listByType'>>(LISTBYTYPE, {
+  const { data, loading } = useQuery<IQueryFilter<'listByType'>>(LISTBYTYPE, {
     variables: {
       type: ['artists', 'albums', 'playlist', 'tracks'],
       limit: 25
@@ -29,40 +30,46 @@ const Public: NextPageFCProps = () => {
           gap: 10px;
         `}
       >
-        {typeSearch?.map((item) => (
+        {loading ? (
+          <AtomLoader type="small" isLoading colorLoading="white" />
+        ) : (
           <>
-            <AtomText color="white" fontWeight="bold">
-              {item}
-            </AtomText>
-            <AtomWrapper
-              width="100%"
-              flexDirection="row"
-              flexWrap="wrap"
-              customCSS={css`
-                display: flex;
-                gap: 10px;
-              `}
-            >
-              {data?.listByType?.[item as keyof typeof data.listByType]?.map(
-                (props) => (
-                  <AtomCard
-                    key={props?.id}
-                    {...props}
-                    image={props?.images?.[0]?.url as string}
-                    onClick={() => {
-                      router.push({
-                        pathname: `/public/${props?.type}/[id]`,
-                        query: {
-                          id: props?.id
-                        }
-                      });
-                    }}
-                  />
-                )
-              )}
-            </AtomWrapper>
+            {typeSearch?.map((item) => (
+              <>
+                <AtomText color="white" fontWeight="bold">
+                  {item}
+                </AtomText>
+                <AtomWrapper
+                  width="100%"
+                  flexDirection="row"
+                  flexWrap="wrap"
+                  customCSS={css`
+                    display: flex;
+                    gap: 10px;
+                  `}
+                >
+                  {data?.listByType?.[
+                    item as keyof typeof data.listByType
+                  ]?.map((props) => (
+                    <AtomCard
+                      key={props?.id}
+                      {...props}
+                      image={props?.images?.[0]?.url as string}
+                      onClick={() => {
+                        router.push({
+                          pathname: `/public/${props?.type}/[id]`,
+                          query: {
+                            id: props?.id
+                          }
+                        });
+                      }}
+                    />
+                  ))}
+                </AtomWrapper>
+              </>
+            ))}
           </>
-        ))}
+        )}
       </AtomWrapper>
     </AtomWrapper>
   );
